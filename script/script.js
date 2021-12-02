9ccc41a5285f3184f70be22956a082aa574bcae4
Olesia-Kravchenko193 <yaseloklass@gmail.com>
2021-12-03 00:34:38 +0200

    исправления

--- script/script.js
+++ script/script.js
@@ -8,6 +8,7 @@ const boxWidth = 200;
 const boxHeight = 200;
 
 let boxes = [];
+let boxesTextBox = [];
 let action = false;
 let $selectedBox = null;
 let selectedBoxIndex = null;
@@ -25,53 +26,52 @@ let distance = {
     y: 0
 }
 
-if (localStorage.getItem('coords')) {
+if (!!localStorage.getItem('coords')) {
     boxes = JSON.parse(localStorage.getItem('coords'));
-    boxGenerator(boxes);
+    boxesTextBox = JSON.parse(localStorage.getItem('textBox'));
+    boxGenerator(boxes, boxesTextBox);
 }
 
 
-function boxGenerator() {
+function boxGenerator(boxesList, boxesTextList) {
     let template = '';
-    for (let i = 0; i < boxes.length; i++) {
-        template += '<div class="box" data-index="' + i + '" style="transform: translate(' + boxes[i].x + 'px, ' + 
-        boxes[i].y + 'px)"><textarea rows="12" cols="22" data-index="' + i + '"> ' + boxes[i].text + '</textarea></div>';
+    for (let i = 0; i < boxesList.length; i++) {
+        template += '<div class="box" id="' + String(i) + "box" + '" style="transform: translate(' + boxesList[i].x + 'px, ' + boxesList[i].y + 'px)">';
+        template += '<textarea class="textArea" placeholder="Введите данные" rows="12" cols="22" id="' + String(i) + "textarea" + '"> ' + boxesTextList[i] + '</textarea></div>';
     }
-    console.log(template);
     $area.innerHTML = template;
+    console.log($area);
 }
 
 function boxController(coords) {
     $selectedBox.style.cssText = 'transform: translate(' + coords.x + 'px, ' + coords.y + 'px)';
 }
 
+$area.addEventListener('click', function(e) {
+    if(!!e.target.classList.contains('textArea')){
+        e.target.addEventListener('input', function (e) {
+            boxesTextBox[e.target.id.slice(0, 1)] = document.getElementById(`${String(e.target.id.slice(0, 1)) + "textarea"}`).value;
+            localStorage.setItem('textBox', JSON.stringify(boxesTextBox));
+        })
+    }
+});
+
 $area.addEventListener('mousedown', function (e) {
     if (!!e.target.classList.contains('box')) {
         action = true;
         $selectedBox = e.target;
-        selectedBoxIndex = parseFloat($selectedBox.getAttribute('data-index'));
-
+        selectedBoxIndex = e.target.id;
         startCoords.x = e.clientX;
         startCoords.y = e.clientY;
     }
     
 });
 
-$area.addEventListener('input', function(e) {
-    $selectedBox = e.target;
-    let sBoxIndex = parseFloat($selectedBox.getAttribute('data-index'));
-    boxes[sBoxIndex] = {
-        x: boxes[sBoxIndex].x,
-        y: boxes[sBoxIndex].y,
-        text: e.target.value
-    };
-});
-
 $area.addEventListener('mouseup', function (e) {
-    if (!e.target.classList.contains('add-btn')) {
+    if (!!e.target.classList.contains('box')) {
         action = false;
-        boxes[selectedBoxIndex].x = distance.x;
-        boxes[selectedBoxIndex].y = distance.y;
+        boxes[selectedBoxIndex.slice(0, 1)].x = distance.x;
+        boxes[selectedBoxIndex.slice(0, 1)].y = distance.y;
         localStorage.setItem('coords', JSON.stringify(boxes));
     }
 
@@ -82,8 +82,8 @@ $area.addEventListener('mousemove', function (e) {
         currentCoords.x = e.clientX;
         currentCoords.y = e.clientY;
 
-        distance.x = boxes[selectedBoxIndex].x + (currentCoords.x - startCoords.x);
-        distance.y = boxes[selectedBoxIndex].y + (currentCoords.y - startCoords.y);
+        distance.x = boxes[selectedBoxIndex.slice(0, 1)].x + (currentCoords.x - startCoords.x);
+        distance.y = boxes[selectedBoxIndex.slice(0, 1)].y + (currentCoords.y - startCoords.y);
 
         if (distance.x >= (areaWidth - boxWidth)) distance.x = areaWidth - boxWidth;
         if (distance.x <= 0) distance.x = 0;
@@ -96,10 +96,21 @@ $area.addEventListener('mousemove', function (e) {
 });
 
 $btn.addEventListener('click', function () {
-    boxes.push({
-        x: 0,
-        y: 0,
-        text: "Text№" + (boxes.length + 1)
-    });
-    boxGenerator(boxes);
+    if (!!boxes.length) {
+        boxes.push({
+            x: 0,
+            y: 0
+         })
+        boxesTextBox.push("");
+    } else {
+        boxes = [{
+            x: 0,
+            y: 0
+        }];
+        boxesTextBox = [""];
+    }
+    boxGenerator(boxes, boxesTextBox);
+
+    localStorage.setItem('coords', JSON.stringify(boxes));
+    localStorage.setItem('textBox', JSON.stringify(boxesTextBox));
 });
